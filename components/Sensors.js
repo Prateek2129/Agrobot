7454c2c8c33d57dff5532cbe9ce884c30f135315
Prateek2129 <87858021+Prateek2129@users.noreply.github.com>
2025-08-04 10:15:11 +0530

    Create Sensors.js

---
+++ components/Sensors.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Sensors = () => {
+  return (
+    <div>
+      <h1>Sensors</h1>
+      <ul>
+        <li>Distance Sensor: Active</li>
+        <li>Soil Sensor: Active</li>
+        <li>Mapping Sensor: Active</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sensors;
